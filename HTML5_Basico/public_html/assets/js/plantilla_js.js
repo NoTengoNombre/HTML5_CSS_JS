c8feb5e2237754774e0187e0b3abb424c3863a6d
spacemonkey <siguelasreglas@gmail.com>
2017-06-22 15:50:22 +0200

    Secciones Html5

---
+++ HTML5_Basico/public_html/assets/js/plantilla_js.js
@@ -0,0 +1,10 @@
+/**
+ @Created on : 22-jun-2017, 12:11:06
+ @Author     : RVS
+ @Pag        :
+ @version    :
+ */
+
+function mi_funcion() {
+    alert('Funcion de Javascript');
+}
---
+++ HTML5_Basico/public_html/mclibre/2_lista_de_etiquetas_html5_1/plantilla_js.js
@@ -0,0 +1,16 @@
+/**
+ @Created on : 22-jun-2017, 12:11:06
+ @Author     : RVS
+ @Pag        :
+ @version    :
+ */
+
+//Funcion anonima
+//(function () {
+//    alert('funcion externa');
+//})();
+
+
+function mi_funcion() {
+    alert('Funcion de Javascript');
+}
